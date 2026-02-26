f245012d38ef9b1aa788b1cb18a6f11b2d424208
reyhansaja <raihan.ts16b@gmail.com>
2026-02-26 09:48:16 +0700

    tambah pratikum 4

--- Minggu2/PWL_Minggu2/routes/web.php
+++ Minggu2/PWL_Minggu2/routes/web.php
@@ -47,3 +47,4 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
