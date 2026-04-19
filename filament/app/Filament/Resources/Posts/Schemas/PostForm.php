32bc8e2ccd34e1b93bd808707a08214450122519
reyhansaja <raihan.ts16b@gmail.com>
2026-04-20 01:51:16 +0700

    JS 6-2

--- filament/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ filament/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -2,15 +2,17 @@
 
 namespace App\Filament\Resources\Posts\Schemas;
 
-use Filament\Schemas\Schema;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
 
 class PostForm
 {
@@ -18,19 +20,38 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make('title')->required(),
-                TextInput::make('slug')->required(),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->required(),
-                ColorPicker::make('color')->required(),
-                MarkdownEditor::make('content')->required(),
-                FileUpload::make('image')
-                ->disk('public')
-                ->directory('posts'),
-                TagsInput::make("tags"),
-                Checkbox::make('published'),
-                DateTimePicker::make('published_at'),
-            ]);
+                Section::make('Post Details')
+                    ->description('Fill in the details of the post')
+                    ->icon('heroicon-o-document-text')
+                    ->columnSpan(2)
+                    ->schema([
+                        TextInput::make('title'),
+                        TextInput::make('slug'),
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->preload()
+                            ->searchable()
+                            ->required(),
+                        ColorPicker::make('color'),
+                        MarkdownEditor::make('content'),
+                    ]),
+
+                Group::make([
+                    Section::make('Image Upload')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->disk('public')
+                                ->directory('posts'),
+                        ]),
+
+                    Section::make('Meta Information')
+                        ->schema([
+                            TagsInput::make('tags'),
+                            Checkbox::make('published'),
+                            DateTimePicker::make('published_at'),
+                        ]),
+                ])->columnSpan(1),
+            ])
+            ->columns(3);
     }
 }
