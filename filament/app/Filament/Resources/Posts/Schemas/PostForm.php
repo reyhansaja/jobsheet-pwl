561d376ff7a77afe56d100170507accbf7c4f391
reyhansaja <raihan.ts16b@gmail.com>
2026-04-20 07:16:14 +0700

    js 6-3

--- filament/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ filament/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -20,30 +20,55 @@ class PostForm
     {
         return $schema
             ->components([
+                // Section 1 - post details
                 Section::make('Post Details')
                     ->description('Fill in the details of the post')
                     ->icon('heroicon-o-document-text')
-                    ->columnSpan(2)
                     ->schema([
-                        TextInput::make('title'),
-                        TextInput::make('slug'),
-                        Select::make('category_id')
-                            ->relationship('category', 'name')
-                            ->preload()
-                            ->searchable()
-                            ->required(),
-                        ColorPicker::make('color'),
+                        // grouping fields into 2 columns
+                        Group::make([
+                            TextInput::make('title')
+                                ->rules(['required', 'min:5'])
+                                ->validationMessages([
+                                    'required' => 'Title wajib diisi.',
+                                    'min' => 'Title minimal 5 karakter.',
+                                ]),
+                            TextInput::make('slug')
+                                ->rules(['required', 'min:3'])
+                                ->unique()
+                                ->validationMessages([
+                                    'required' => 'Slug wajib diisi.',
+                                    'min' => 'Slug minimal 3 karakter.',
+                                    'unique' => 'Slug harus unik.',
+                                ]),
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Category wajib dipilih.',
+                                ])
+                                ->preload()
+                                ->searchable(),
+                            ColorPicker::make('color'),
+                        ])->columns(2),
                         MarkdownEditor::make('content'),
-                    ]),
+                    ])->columnSpan(2),
 
+                // Grouping fields into 2 columns
                 Group::make([
+                    // section 2 - image
                     Section::make('Image Upload')
                         ->schema([
                             FileUpload::make('image')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Image wajib diupload.',
+                                ])
                                 ->disk('public')
                                 ->directory('posts'),
                         ]),
 
+                    // section 3 - meta
                     Section::make('Meta Information')
                         ->schema([
                             TagsInput::make('tags'),
